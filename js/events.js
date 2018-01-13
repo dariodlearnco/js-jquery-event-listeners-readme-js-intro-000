6f602852af415799951fac9ff66d80852515a8df
Dario Dorella <dario.dorella@gmail.com>
2018-01-13 00:35:10 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -23,15 +23,19 @@ function pressIt() {
     // Does not accept a parameter. The function should bind a keydown event to
     // the input field of the form that alerts a user when they have pressed the
     // G key.
+}
 
 function submitIt() {
     // Does not accept a parameter. The function should bind a submit event to
     // the form that alerts "Your form is going to be submitted now.".
+}
 
 $(document).ready(function(){
 
     // call functions here
     getIt()
     frameIt()
+    pressIt()
+    submitIt()
 
 });
